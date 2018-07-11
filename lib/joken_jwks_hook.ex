0506097755a0a2596e0e9577a02dba994b531295
Victor Oliveira Nascimento <victor.olinasc@gmail.com>
2018-07-11 14:03:19 -0300

    Fix return tuples

--- lib/joken_jwks_hook.ex
+++ lib/joken_jwks_hook.ex
@@ -6,19 +6,22 @@ defmodule JokenJwksHook do
   @impl true
   def before_verify(options, token, _signer) do
     with {:ok, signer} <- fetch_signer(options) do
-      {:ok, options, token, signer}
+      {:ok, token, signer}
+    else
+      error ->
+        {:halt, error}
     end
   end
 
   defp fetch_signer(opts = [jwks_url: _url, key_id: _id]) do
     with {:ok, signer} <- Cachex.fetch(@cache, :signer, fn _ -> {:error, :missing} end) do
-      signer
+      {:ok, signer}
     else
       {:error, :missing} ->
         {:ok, signer} = JokenJwksHook.Fetcher.fetch_signer(opts)
 
         if is_nil(signer) do
-          {:error, :no_signer}
+          {:halt, :could_not_signer}
         else
           Cachex.put(@cache, :signer, signer)
           {:ok, signer}
