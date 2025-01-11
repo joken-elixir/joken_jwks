4e6972f0db62fee4bb28dbdefd1828eee815284b
Victor Oliveira Nascimento <victor.olinasc@gmail.com>
2025-01-11 13:13:30 -0300

    Bump RC

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule JokenJwks.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/joken-elixir/joken_jwks"
-  @version "1.7.0-rc.0"
+  @version "1.7.0-rc.1"
 
   def project do
     [
